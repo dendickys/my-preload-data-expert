296fc8fb2a37785810b7a9e1cc64d1ed79f131c9
dendickys <febri.unquad2014@gmail.com>
2019-10-19 09:44:42 +0700

    Add Class MahasiswaModel

---
+++ app/src/main/java/com/example/mypreloaddataexpert/model/MahasiswaModel.java
@@ -0,0 +1,34 @@
+package com.example.mypreloaddataexpert.model;
+
+public class MahasiswaModel {
+    private int id;
+    private String name;
+    private String nim;
+
+    public MahasiswaModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+}
