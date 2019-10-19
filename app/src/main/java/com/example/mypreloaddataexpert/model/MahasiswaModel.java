0aa30b774be5165d686019827c7ecb54457bf32f
dendickys <febri.unquad2014@gmail.com>
2019-10-19 09:45:47 +0700

    Implements Parcelable on model

--- app/src/main/java/com/example/mypreloaddataexpert/model/MahasiswaModel.java
+++ app/src/main/java/com/example/mypreloaddataexpert/model/MahasiswaModel.java
@@ -1,6 +1,9 @@
 package com.example.mypreloaddataexpert.model;
 
-public class MahasiswaModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MahasiswaModel implements Parcelable {
     private int id;
     private String name;
     private String nim;
@@ -31,4 +34,34 @@ public class MahasiswaModel {
     public void setNim(String nim) {
         this.nim = nim;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.name);
+        dest.writeString(this.nim);
+    }
+
+    private MahasiswaModel(Parcel in) {
+        this.id = in.readInt();
+        this.name = in.readString();
+        this.nim = in.readString();
+    }
+
+    public static final Parcelable.Creator<MahasiswaModel> CREATOR = new Parcelable.Creator<MahasiswaModel>() {
+        @Override
+        public MahasiswaModel createFromParcel(Parcel source) {
+            return new MahasiswaModel(source);
+        }
+
+        @Override
+        public MahasiswaModel[] newArray(int size) {
+            return new MahasiswaModel[size];
+        }
+    };
 }
